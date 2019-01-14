ebdee6fcaf8bdfc97babcad50acb65768c683d72
JoonJang95 <joon.jang95@gmail.com>
2019-01-14 00:50:40 -0500

    Clean up comments in sdc seed file

--- database/SDC_seed.js
+++ database/SDC_seed.js
@@ -1,22 +1,10 @@
 const faker = require('faker');
-// const fs = require('fs');
-// const path = require('path');
 const stream = require('stream');
 const mongoose = require('mongoose');
-// const Promise = require('bluebird');
-// const JSONStream = require('JSONStream');
-// const { streamToMongoDB } = require('stream-to-mongo-db');
+
 let readable = new stream.Readable(); // new empty stream.Readable
 
-// const outputDBconfig = {
-//   dbURL: 'mongodb://localhost:27017/sdcRelatedSongs',
-//   collection: 'songs',
-//   batchSize: 10000,
-// };
-// const writableStream = streamToMongoDB(outputDBconfig);
 const { Playlists, Albums, Songs } = require('./SDC_database.js');
-
-// Name of Collection and creation of documents (ONLY USE TO TEST MONGOEXPORT)
 
 const audiofile = [
   'https://s3.amazonaws.com/audiosdc/Audio+Files/bensound-buddy.mp3',
@@ -56,13 +44,6 @@ const dataSeeder = () => {
     if (i % 10000 === 0) {
       const data = JSON.stringify(song);
       readable.push(data);
-      // else if (round === 2) {
-      //   readable2.push(data);
-      // }
-
-      // console.log('100 boi');
-
-      // number += 1;
       song = [];
     }
     if (i % 500000 === 0) {
@@ -72,10 +53,6 @@ const dataSeeder = () => {
     if (i === 1000000) {
       console.log('going to push null');
       readable.push(null);
-
-      // else if (round === 2) {
-      //   readable2.push(null);
-      // }
     }
   }
 
@@ -117,65 +94,9 @@ const dataSeeder = () => {
       }
     });
   });
-
-  // } else if (round === 2) {
-  //   readable2.on('data', (chunk) => {
-  //     number += 1;
-  //     console.log(number);
-  //   });
-  // }
 };
 console.time('streamTime');
 dataSeeder();
-
-// return JSON.stringify(arr);
-// }
-
-// piping
-// readable.pipe(JSONStream.parse('*')).pipe(writableStream);
-// readable.on('end', () => {
-//   console.log('done');
-//   console.timeEnd('streamTime');
-//   mongoose.disconnect();
-// });
-
-// through the data event
-
-// readable.on('readable', () => {
-//   // there is some data to read now
-//   let data;
-
-//   while ((data = this.read(1000))) {
-//     console.log(data);
-//   }
-// });
-
-// WORKING METHOD UNCOMMENT
-// let number = 0;
-// readable.on('data', (chunk) => {
-//   number += 1;
-//   Songs.collection.insertMany(JSON.parse(chunk.toString()), (err) => {
-//     if (err) {
-//       console.log('error inserting documents in to songs collection');
-//     } else {
-//       // console.log('multiple documents inserted to songs collection');
-//       // console.log('Time for Songs seeding');
-//       // console.timeEnd('seedingSongs');
-//       mongoose.disconnect(() => {
-//         // console.log('disconnected!');
-//         number += 1;
-//       });
-//     }
-//     if (number === 100) {
-//       console.log(number);
-//       console.timeEnd('streamTime');
-//     }
-//   });
-// });
-// readable.on('end', () => {
-//   console.log(number);
-//   console.timeEnd('streamTime');
-// });
 
 // mongoose.connection.once('open', () => {
 //   console.log('mongoose is now Seeding!!!');
@@ -204,81 +125,8 @@ dataSeeder();
 //     if (!err) {
 //       console.log('dropped songs collections');
 //     }
-//     console.log('Timer Started');
-//     console.time('seedingAlbum');
-//     console.time('seedingPlaylists');
-//     console.time('seedingSongs');
 //   });
 // });
-
-// Generate multiple documents
-
-// multiple playlists
-
-// const allPlaylists = (() => {
-//   const playlists = [];
-//   for (let i = 1; i <= 200000; i += 1) {
-//     playlists.push({
-//       title: faker.lorem.words(),
-//       owner: `${faker.name.firstName()} ${faker.name.lastName()}`,
-//       description: faker.lorem.words(),
-//       imageURL: faker.random.image(),
-//       likes: faker.random.number(),
-//       shares: faker.random.number(),
-//     });
-//   }
-//   return playlists;
-// })();
-
-// // multiple Albums
-
-// const allAlbums = (() => {
-//   const albums = [];
-//   for (let i = 1; i <= 300000; i += 1) {
-//     albums.push({
-//       name: faker.lorem.words(),
-//       artist: `${faker.name.firstName()} ${faker.name.lastName()}`,
-//       year: 2017,
-//       imageURL: faker.random.image(),
-//     });
-//   }
-//   return albums;
-// })();
-
-// Multiple songs
-
-// const allSongs = () => {
-//   const songs = [];
-//   for (let i = 1; i <= 1; i += 1) {
-//     songs.push({
-//       title: faker.lorem.words(),
-//       genre: genre[randomNum()],
-//       playList: [randomNum(), randomNum(), randomNum()],
-//       artist: 'hey',
-//       albumID: 1,
-//       released: faker.date.past(),
-//       duration: faker.random.number(),
-//       image: faker.random.image(),
-//       songURL: audiofile[randomNum()],
-//       plays: faker.random.number(),
-//       likes: faker.random.number(),
-//       shares: faker.random.number(),
-//       comments: faker.random.number(),
-//     });
-// if (i % 1 === 0) {
-// readable.push(JSON.stringify(songs))
-
-// readable.pipe(process.stdout);
-// console.log('500k hit bruh');
-// songs = [];
-// if (i % 500 === 0) {
-//   console.log('5M');
-// }
-// readable.pipe(process.stdout);
-// }
-//   }
-//   return songs;
-// };
 
 // Seed DB w/ Nodejs to MongoDB
 
